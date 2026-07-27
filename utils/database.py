4825fdaaa40f80118fa09e94517166eeb848d833
NOTZ112 <liza3x3i@gmail.com>
2026-07-28 02:54:30 +0530

    Update database.py

--- utils/database.py
+++ utils/database.py
@@ -16,45 +16,59 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #=========================================================================
 
-from .info import * 
+from .info import DATABASE_URI
 from pymongo import MongoClient
 
 dbclient = MongoClient(DATABASE_URI)
-db       = dbclient["Auto-Delete"]
-col      = db["DATA"]
 
-def save_message(message, time):
-    data = {"chat_id": message.chat.id,
-            "message_id": message.id,
-            "time": time}
-    col.insert_one(data)
-   
-def get_all_data(time):
-    data     = {"time":{"$lte":time}}
-    all_data = list(col.find(data))
-    return all_data
+db = dbclient["AutoDelete"]
 
-def save_message(chat_id, message_id, delete_time):
-    data = {
-        "chat_id": chat_id,
-        "message_id": message_id,
-        "time": delete_time
-    }
-    col.insert_one(data)
+messages = db["MESSAGES"]
 groups = db["GROUPS"]
 
 
+# ---------------- Messages ---------------- #
+
+def save_message(chat_id, message_id, delete_time):
+    messages.insert_one({
+        "chat_id": chat_id,
+        "message_id": message_id,
+        "time": delete_time
+    })
+
+
+def get_all_data(current_time):
+    return list(messages.find({
+        "time": {
+            "$lte": current_time
+        }
+    }))
+
+
+def delete_all_data(all_data):
+    for data in all_data:
+        messages.delete_one({
+            "_id": data["_id"]
+        })
+
+
+# ---------------- Groups ---------------- #
+
 def get_group(chat_id):
-    return groups.find_one({"chat_id": chat_id})
+    return groups.find_one({
+        "chat_id": chat_id
+    })
 
 
-def enable_group(chat_id, delete_time=10):
+def enable_group(chat_id):
     groups.update_one(
-        {"chat_id": chat_id},
+        {
+            "chat_id": chat_id
+        },
         {
             "$set": {
                 "enabled": True,
-                "time": delete_time
+                "time": 10
             }
         },
         upsert=True
@@ -63,7 +77,9 @@ def enable_group(chat_id, delete_time=10):
 
 def disable_group(chat_id):
     groups.update_one(
-        {"chat_id": chat_id},
+        {
+            "chat_id": chat_id
+        },
         {
             "$set": {
                 "enabled": False
@@ -73,12 +89,14 @@ def disable_group(chat_id):
     )
 
 
-def set_group_time(chat_id, delete_time):
+def set_group_time(chat_id, seconds):
     groups.update_one(
-        {"chat_id": chat_id},
+        {
+            "chat_id": chat_id
+        },
         {
             "$set": {
-                "time": delete_time
+                "time": seconds
             }
         },
         upsert=True
